1bdd8ce47f33a4e4a80805c83af4d92d0eaf359f
Lidan-Huang <huangld0314@gmail.com>
2022-06-30 22:17:02 -0700

    added Vehicle class

--- ooChallenge.js
+++ ooChallenge.js
@@ -1,1 +1,26 @@
 "use strict";
+
+/**Create a class for vehicle with three properties and two methods:
+ * - make, model and year
+ * - honk method: returns string "Beep."
+ * - toString method: returns string, like:
+ *  - let myFirstVehicle = new Vehicle("Honda", "Monster Truck", 1999);
+ *  - myFirstVehicle.toString(); 
+ *  - returns: "The vehicle is a Honda Monster Truck from 1999."
+ */
+
+class Vehicle {
+  constructor (make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  honk () {
+    return "Beep.";
+  }
+
+  toString () {
+    return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`
+  }
+}
